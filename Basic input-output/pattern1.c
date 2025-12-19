6d3745fe9be80534862e720ed98f3126dc98f140
gauravsingh22-del <gr0757931@gmail.com>
2025-12-19 17:56:39 +0530

    Basic input-output C programs

---
+++ Basic input-output/pattern1.c
@@ -0,0 +1,23 @@
+//Show the patterns just using printf statements:
+#include<stdio.h>
+int main(){
+    printf("\nRight half pyramid:");
+    printf("\n*");
+    printf("\n* *");
+    printf("\n* * *");
+    printf("\n* * * *");
+    printf("\n* * * * *");
+    printf("\nReverse right Half Pyramid");
+    printf("\n* * * * *");
+    printf("\n* * * * ");
+    printf("\n* * * ");
+    printf("\n* * ");
+    printf("\n* ");
+    printf("\n Left half Pyramid");
+    printf("        *\n");
+    printf("      * *\n");
+    printf("    * * *\n");
+    printf("  * * * *\n");
+    printf("* * * * *\n");
+
+}
