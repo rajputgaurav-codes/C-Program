c586904d454970ed6ac776867e1c38aa57dcd9a0
gauravsingh22-del <gr0757931@gmail.com>
2025-12-22 22:15:16 +0530

    day3

---
+++ Apnacollage/1.c
@@ -0,0 +1,13 @@
+//write a program to input two number and display its sum.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number= ");
+    scanf("%d",&a);
+    printf("enter second number= ");
+    scanf("%d",&b);
+    //sum of both number
+    printf("Sum= %d",a+b);
+    return 0;
+
+}
