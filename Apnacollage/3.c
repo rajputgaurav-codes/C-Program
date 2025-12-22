18a8a27ca559010e7b550527ab217435b18d5a2a
gauravsingh22-del <gr0757931@gmail.com>
2025-12-22 22:36:09 +0530

    day3

---
+++ Apnacollage/3.c
@@ -0,0 +1,9 @@
+/*Create a program to declare two integer variables,
+assign them values,and display their values.*/
+#include<stdio.h>
+int main(){
+ int a=24,b=34;
+ printf("enter first number=%d\n",a);
+ printf("enter second number=%d",b);
+ return 0;
+}
