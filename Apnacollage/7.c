d550bc1477629d6894b0a29d8b1c2f49a7049dbf
gauravsingh22-del <gr0757931@gmail.com>
2025-12-23 16:59:44 +0530

    day4

---
+++ Apnacollage/7.c
@@ -0,0 +1,9 @@
+//Take a number(n)from the user & output its cube(n*n*n).
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number=");
+    scanf("%d",&n);
+    printf("Cube=%d",n*n*n);
+    return 0;
+}
