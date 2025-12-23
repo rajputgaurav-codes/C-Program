e6f270bb74ab7e663498d3fcbf229c34fc8983e4
gauravsingh22-del <gr0757931@gmail.com>
2025-12-23 16:41:27 +0530

    day4

---
+++ Apnacollage/5.c
@@ -0,0 +1,9 @@
+//write a program to calulate area of a circle.
+#include<stdio.h>
+int main(){
+    int radious;
+    printf("The radious of circle is=");
+    scanf("%d",&radious);
+    printf("Area of Circle=%f",3.14*(radious*radious));
+    return 0;
+}
