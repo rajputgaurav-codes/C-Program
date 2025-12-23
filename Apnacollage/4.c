799616996c2f7a31393b3550f5392f4fce5aa171
gauravsingh22-del <gr0757931@gmail.com>
2025-12-23 16:40:28 +0530

    day4

---
+++ Apnacollage/4.c
@@ -0,0 +1,8 @@
+//write a program to calulate area of a square.(side is given)
+#include<stdio.h>
+int main(){
+    int side=24;
+    printf("\nenter a value of side=%d ",24);
+    printf("\nArea of Square=%d",side*side);
+    return 0;
+}
