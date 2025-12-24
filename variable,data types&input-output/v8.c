cb2d72d03054b0dfc1e67a4d6f7523b472df8c6c
gauravsingh22-del <gr0757931@gmail.com>
2025-12-24 18:46:29 +0530

    day5

---
+++ variable,data types&input-output/v8.c
@@ -0,0 +1,9 @@
+//Write a C program to input marks (out of 100) and display them in the given format.
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("enter a marks (out of 100)=");
+    scanf("%d",&marks);
+    printf("Marks=%d",marks);
+    return 0;
+}
