b43418b75a8ebaf8c1f122637b670f73a8ffaf4e
gauravsingh22-del <gr0757931@gmail.com>
2025-12-24 18:45:44 +0530

    day5

---
+++ variable,data types&input-output/v6.c
@@ -0,0 +1,11 @@
+//Write a C program to input length and breadth of a rectangle and calculate its area.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a length=");
+    scanf("%d",&a);
+    printf("enter a breadth=");
+    scanf("%d",&b);
+    printf("Area=%d",a*b);
+    return 0;
+}
