6c812028854a02789a992a8de523a64ff94b283e
gauravsingh22-del <gr0757931@gmail.com>
2025-12-24 17:32:42 +0530

    day5

---
+++ variable,data types&input-output/v1.c
@@ -0,0 +1,9 @@
+//write a c progtram to input an integer and print its square.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number=");
+    scanf("%d",&n);
+    printf("Suare =%d",n*n);
+    return 0;
+}
