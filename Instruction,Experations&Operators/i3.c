7a8836efe20f0e1e7554a82982d1b11dc94f727c
gauravsingh22-del <gr0757931@gmail.com>
2025-12-26 22:26:02 +0530

    day7

---
+++ Instruction,Experations&Operators/i3.c
@@ -0,0 +1,16 @@
+//create a program to calculate peimeter of a rectangle.
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("enter a value of first side=");
+    scanf("%d",&a);
+    printf("enter a value of second side=");
+    scanf("%d",&b);
+    printf("enter a value of third side=");
+    scanf("%d",&c);
+    printf("enter a value of fourth side=");
+    scanf("%d",&d);
+    printf("Perimeter of a rectangle= %d\n ",a+b+c+d);
+    return 0;
+
+}
