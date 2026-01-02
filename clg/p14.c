180ce8d1300065e354f80e96091cbe0377aaf451
gauravsingh22-del <gr0757931@gmail.com>
2026-01-02 21:01:12 +0530

    add loop program

---
+++ clg/p14.c
@@ -0,0 +1,9 @@
+//write a program that input number and display following series 1,4,9,16,25.
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=1;i<=5;i++){
+        printf("%d",i*i);
+    }
+    return 0;
+}
