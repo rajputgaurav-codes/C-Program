bcd3750a263d0274909e1860f8858bae705d2f37
gauravsingh22-del <gr0757931@gmail.com>
2026-01-05 22:09:03 +0530

    loop program

---
+++ clg/p23.c
@@ -0,0 +1,19 @@
+//write a peogram that input multiple numbers ignored negative numbers.input numbers till user enter 0. finally display sum of all positinive numbers.
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    printf("enter a number=");
+    scanf("%d",&n);
+    while(1){
+        if(n<0){
+            continue;
+        }if(n==0){
+             break;
+        }
+        if(n<0){
+        sum=sum+n;
+        }
+    }
+    printf("sum=%d\n",sum);
+    return 0;
+}
