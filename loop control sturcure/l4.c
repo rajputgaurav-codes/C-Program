4d2acd559c691d3f7dbe3beca6f8ea17a66c2637
gauravsingh22-del <gr0757931@gmail.com>
2026-01-06 21:54:59 +0530

    loop program

---
+++ loop control sturcure/l4.c
@@ -0,0 +1,15 @@
+//create a program that computes the sum of the digits of an integer.
+#include<stdio.h>
+int main(){
+    int i,n,s=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(;n>0;){
+       
+        s=s+n%10;
+         n=n/10;
+        
+    }
+    printf("%d\n",s);
+    return 0;
+}
