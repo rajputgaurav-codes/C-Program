1d1596ec8fc184ced3ca4207566325447dfa5503
gauravsingh22-del <gr0757931@gmail.com>
2026-01-08 23:03:51 +0530

    day 1

---
+++ Daily code/1.c
@@ -0,0 +1,13 @@
+//add tow number.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a first number=");
+    scanf("%d",&a);
+    printf("enter a second number=");
+    scanf("%d",&b);
+    int s=0;
+    s=a+b;
+    printf("Sum=%d",s);
+    return 0;
+}
