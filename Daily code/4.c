19c9714d16e611644044fc3d82049eecdafb5895
gauravsingh22-del <gr0757931@gmail.com>
2026-01-08 23:22:14 +0530

    day 1

---
+++ Daily code/4.c
@@ -0,0 +1,12 @@
+//area of circle.
+#include<stdio.h>
+int main(){
+    int r;
+    float c;
+    printf("enter a value of radious=");
+    scanf("%d",&r);
+    c=(float)3.14*(r*r);
+    printf("Area of circule=%.2f",c);
+    return 0;
+
+}
