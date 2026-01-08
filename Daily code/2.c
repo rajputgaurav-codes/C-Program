b8e9325602fc92c6e90a54294b6329692ee204af
gauravsingh22-del <gr0757931@gmail.com>
2026-01-08 23:21:39 +0530

    day 1

---
+++ Daily code/2.c
@@ -0,0 +1,11 @@
+//area of rectangle.
+#include<stdio.h>
+int main(){
+    int l,w;
+    printf("enter a value of length=");
+    scanf("%d",&l);
+    printf("enter a value of width=");
+    scanf("%d",&w);
+    printf("Area of Rectangle=%d",l*w);
+    return 0;
+}
