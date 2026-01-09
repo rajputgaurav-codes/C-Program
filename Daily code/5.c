d1875b445fc3f3e5c7d454e28bf6e05e90fac539
gauravsingh22-del <gr0757931@gmail.com>
2026-01-09 21:15:39 +0530

    find the reminder of two number.

---
+++ Daily code/5.c
@@ -0,0 +1,16 @@
+//find the reminder of two number.
+#include<stdio.h>
+int main(){
+    // int a,b,rem;
+    // printf("enter tow number=");
+    // scanf("%d,%d",&a,&b);
+    // rem=a%b;
+    // printf("reminder is=%d",rem);
+    // return 0;
+    // int a=5;
+    // printf("%d",a++);
+    // return 0;
+    int a=5;
+    printf("%d",++a);
+    return 0;
+}
