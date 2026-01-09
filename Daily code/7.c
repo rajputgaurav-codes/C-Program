b33cdcddecc292f9e216cfca64fce2642b558608
gauravsingh22-del <gr0757931@gmail.com>
2026-01-09 21:14:28 +0530

    Program using ++ and --

---
+++ Daily code/7.c
@@ -0,0 +1,11 @@
+//Program using ++ and --
+#include<stdio.h>
+int main(){
+    int a=5,b=6;
+    printf(" a=%d\n",a++);
+    printf("after increment=%d\n",a);
+    printf("b=%d\n",b--);
+    printf("after decrement=%d\n",b);
+    return 0;
+
+}
