95d1950da7662c6bbb5e4648062853fc7e6e412d
gauravsingh22-del <gr0757931@gmail.com>
2026-01-12 17:50:19 +0530

    programto evalutate.

---
+++ Daily code/9.c
@@ -0,0 +1,9 @@
+/*Program to evaluate:
+a = 10, b = 5, c = 2
+Expression: a + b * c - a / c */
+#include<stdio.h>
+int main(){
+    int a = 10, b = 5, c = 2;
+    printf("Expression:%d", a + b * c - a / c);
+    return 0;
+}
