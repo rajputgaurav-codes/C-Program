ae9077d86d923075efeaf840c23d84024b32a7b6
gauravsingh22-del <gr0757931@gmail.com>
2026-01-15 18:59:15 +0530

    loop

---
+++ Daily code/19.c
@@ -0,0 +1,17 @@
+//1 to 10 print number.
+#include<stdio.h>
+int main(){
+    // int i;
+    // for(i=1;i<=10;i++){
+    //     printf("%d\n",i);
+    // }
+    // return 0;
+
+
+    int i=1;
+    while(i<=10){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
