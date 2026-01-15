ec7ca38ad3e409ae62dfab3e737408845cb56710
gauravsingh22-del <gr0757931@gmail.com>
2026-01-15 19:38:09 +0530

    pre and post increment

--- Daily code/10.c
+++ Daily code/10.c
@@ -4,7 +4,7 @@ int main(){
     int a;
     printf("enter the value of a:");
     scanf("%d",&a);
-    printf("pre increment:%d\n",a++);
-    printf("post increment:%d\n",++a);
+    printf("post increment:%d\n",a++);
+    printf("pre increment:%d\n",++a);
     return 0;
 }
