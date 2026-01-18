f0b329600d75587616fee427922847b425f130af
gauravsingh22-del <gr0757931@gmail.com>
2026-01-18 22:25:01 +0530

    function to calculate the factorial of a given number

--- loop control sturcure/l3.c
+++ loop control sturcure/l3.c
@@ -2,15 +2,7 @@
 #include<stdio.h>
 int main(){
     int i,n,fact=1;
-    // i=1;
-    // while(i<=n){
-    //     printf("enter a number=");
-    //     scanf("%d",&n);
-    //     fact=fact*i;
-    //     i++;
-    // }
-    // printf("factorial=%d",fact);
-    // return 0;
+    
     printf("enter a number=");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
