2110d6f8649d916a7c0602588a2a6e81b6dd42fb
gauravsingh22-del <gr0757931@gmail.com>
2026-02-24 20:35:58 +0530

    input a number and check the number is absolute or not.

---
+++ Decision Control Structure/d12.c
@@ -0,0 +1,12 @@
+//input a number and check the number is absolute or not.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    if(n<0){
+        n=n*(-1);
+    }
+    printf("The absolute number is : %d",n);
+    return 0;
+}
